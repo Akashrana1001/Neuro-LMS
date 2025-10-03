17c2e21648bb6c0ccec7f884a6482714cc5bb70e
Akash chauhan <116897353+Akashrana1001@users.noreply.github.com>
2025-10-03 09:59:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,7 @@ let port = process.env.PORT || 3000
 let app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use(cors())
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/course", courseRouter)
